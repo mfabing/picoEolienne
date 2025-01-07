3c0da35b826a491d01f05331222b64d1b371414e
Michael Fabing <michaelfabing@hotmail.com>
2025-01-07 15:10:32 -0500

    Update main.py
    add a watchdog extension

--- main.py
+++ main.py
@@ -23,30 +23,36 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, pw)
 
+# Initialize watchdog timer (timeout in milliseconds, e.g., 15000ms = 15 seconds) (maximum is 2^24 clock cycles which is about 16.8 seconds)
+wdt = WDT(timeout=15000)
+
 while True:
     try:
+	# Feed the watchdog to prevent system reset
+        wdt.feed()
+	    
         if wlan.isconnected():
         	adc1_value = adc1.read_u16()
-			#adc2_value = adc2.read_u16()
-			adc3_value = adc3.read_u16()
+		#adc2_value = adc2.read_u16()
+		adc3_value = adc3.read_u16()
 
-			# Send data via HTTP GET
-            url = f"https://jacqueline-michel.com/drapeau/?adc_anemometre={adc1_value}&adc_batteries={adc3_value}"
-            response = requests.get(url)
+		# Send data via HTTP GET
+		url = f"https://jacqueline-michel.com/drapeau/?adc_anemometre={adc1_value}&adc_batteries={adc3_value}"
+		response = requests.get(url)
 
-            # Check HTTP response (to uncomment for debugging purpose)
-            # if response.status_code == 200:
-            #     print("Data sent successfully:", response.text)
-            # else:
-            #     print(f"HTTP Error: {response.status_code}")
-
-			# Blink LED to indicate that the data has been sent success
-            led = Pin(25, Pin.OUT)
-            led.on()
-            sleep(1)
-            led.off()
-
-            sleep(3)  # Pause between iterations
+		# Check HTTP response (to uncomment for debugging purpose)
+		# if response.status_code == 200:
+		#     print("Data sent successfully:", response.text)
+		# else:
+		#     print(f"HTTP Error: {response.status_code}")
+		
+		# Blink LED to indicate that the data has been sent success
+		led = Pin(25, Pin.OUT)
+		led.on()
+		sleep(1)
+		led.off()
+		
+		sleep(3)  # Pause between iterations
 
         else:
             print("WiFi is disconnected. Reconnecting...")
